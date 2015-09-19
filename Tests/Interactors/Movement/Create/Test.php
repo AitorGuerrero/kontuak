1fe0a3c838e9406de236e01de7ffff93a9074454
Aitor Guerrero <aguerrerolasarte@gmail.com>
2015-09-19 12:04:17 +0200

    Tests fixed

--- Tests/Interactors/Movement/Create/Test.php
+++ Tests/Interactors/Movement/Create/Test.php
@@ -5,16 +5,11 @@ namespace kontuak\Tests\Interactors\Movement\Create;
 use Kontuak\Implementation\InMemory\Movement\Factory;
 use Kontuak\Implementation\InMemory\PeriodicalMovement\Factory as PeriodicalMovementFactory;
 use Kontuak\Implementation\InMemory\Movement\Source as MovementSource;
-use Kontuak\Implementation\PeriodicalMovement\Source\InMemory as PeriodicalMovementSource;
-use Kontuak\Implementation\Transformer\Movement as MovementTransformer;
 use Kontuak\Interactors\Movement\Create\UseCase;
 use Kontuak\Interactors\Movement\Create\Request;
 use Kontuak\Interactors\InvalidArgumentException;
-use Kontuak\Interactors\SystemException;
 use Kontuak\Movement;
 use Kontuak\Period;
-use Kontuak\PeriodicalMovement\Id\Generator;
-use Kontuak\PeriodicalMovement\MovementsGenerator;
 
 class Test extends \PHPUnit_Framework_TestCase
 {
@@ -27,41 +22,22 @@ class Test extends \PHPUnit_Framework_TestCase
 
     /** @var Factory */
     private $movementFactory;
-    /** @var MovementsGenerator */
-    public $movementsGenerator;
-    /** @var Generator */
-    public $idGenerator;
     /** @var Request */
     private $request;
     /** @var UseCase */
     private $useCase;
     /** @var MovementSource */
     private $source;
-    /** @var PeriodicalMovementSource */
-    private $periodicalMovementSource;
 
     protected function setUp()
     {
         $this->source = new MovementSource();
-        $this->periodicalMovementSource = new PeriodicalMovementSource();
-        $this->idGenerator = new Generator();
         $this->movementFactory = new Factory();
-        $this->movementsGenerator = new MovementsGenerator(
-            $this->source,
-            new Movement\Id\Generator(),
-            $this->movementFactory,
-            new \DateTime(self::CURRENT_ISO_DATE)
-        );
         $this->periodicalMovementFactory = new PeriodicalMovementFactory();
         $this->useCase = new UseCase(
             $this->source,
-            $this->periodicalMovementSource,
-            $this->idGenerator,
-            $this->movementsGenerator,
             new \DateTime(self::CURRENT_ISO_DATE),
-            new MovementTransformer(),
-            $this->movementFactory,
-            $this->periodicalMovementFactory
+            $this->movementFactory
         );
         
         $this->request = new Request();
@@ -92,21 +68,16 @@ class Test extends \PHPUnit_Framework_TestCase
      */
     public function whenCollectionThrowsAnExceptionShouldThrowASystemException()
     {
-        $entriesCollection = $this
+        $movementsSource = $this
             ->getMockBuilder('Kontuak\Movement\Source')
             ->disableOriginalConstructor()
             ->getMock();
-        $entriesCollection->method('add')->willThrowException(new \Exception());
-        /** @var Movement\Source $entriesCollection */
+        $movementsSource->method('add')->willThrowException(new \Exception());
+        /** @var Movement\Source $movementsSource */
         $useCase = new UseCase(
-            $entriesCollection,
-            $this->periodicalMovementSource,
-            $this->idGenerator,
-            $this->movementsGenerator,
+            $movementsSource,
             new \DateTime(self::CURRENT_ISO_DATE),
-            new MovementTransformer(),
-            $this->movementFactory,
-            $this->periodicalMovementFactory
+            $this->movementFactory
         );
         $useCase->execute($this->request);
     }
@@ -126,49 +97,4 @@ class Test extends \PHPUnit_Framework_TestCase
         $this->assertEquals(self::CURRENT_ISO_DATE, $movement->created()->format('Y-m-d H:i:s'));
     }
 
-    /**
-     * @test
-     */
-    public function shouldReturnMovementInfo()
-    {
-        /** @var Movement $movement */
-        $movement = $this->useCase->execute($this->request);
-
-        $this->assertEquals(self::ID, $movement->id()->serialize());
-        $this->assertEquals(self::AMOUNT, $movement->amount());
-        $this->assertEquals(self::CONCEPT, $movement->concept());
-        $this->assertEquals(self::ISO_DATE, $movement->date()->format('Y-m-d'));
-        $this->assertEquals(self::CURRENT_ISO_DATE, $movement->created()->format('Y-m-d H:i:s'));
-    }
-
-    /**
-     * @test
-     */
-    public function ifHasPeriodInformationShouldCreateAPeriodicalMovement()
-    {
-        /** @var Movement $movement */
-        $this->addPeriodInfoToRequest();
-        $movement = $this->useCase->execute($this->request);
-
-        $this->assertEquals(3, $movement->periodicalMovement()->period()->amount());
-        $this->assertEquals(Period::TYPE_MONTH_DAY, $movement->periodicalMovement()->period()->type());
-    }
-
-    /**
-     * @test
-     */
-    public function evenItIsPeriodicalShouldCreateTheMovement()
-    {
-        $this->addPeriodInfoToRequest();
-        $this->useCase->execute($this->request);
-
-        $this->assertNotNull($this->source->byId(new Movement\Id(self::ID)));
-    }
-
-    private function addPeriodInfoToRequest()
-    {
-        $this->request->isPeriodical = true;
-        $this->request->periodType = Request::PERIOD_TYPE_MONTHS;
-        $this->request->periodAmount = 3;
-    }
 }
