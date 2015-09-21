d80fe409011320dca5479936c5628356812d6509
Aitor Guerrero <aguerrerolasarte@gmail.com>
2015-09-21 21:21:33 +0200

    History use case fixed transformer use

--- Interactors/Movement/History/UseCase.php
+++ Interactors/Movement/History/UseCase.php
@@ -2,13 +2,10 @@
 
 namespace Kontuak\Interactors\Movement\History;
 
-use Kontuak\Interactors\InvalidArgumentException;
 use Kontuak\Movement;
 
 class UseCase
 {
-    /** @var \DateTime */
-    private $today;
     /** @var \Kontuak\Implementation\Transformer\Movement */
     private $movementTransformer;
     /** @var Movement\TotalAmountCalculator */
@@ -36,8 +33,8 @@ class UseCase
             $collection->filterDateLessOrEqualTo(new \DateTime($request->toDate));
         }
         $amounts = $this->calculator->getForACollection($collection, $request->limit);
-        foreach($amounts as $amount) {
-            $amount['movement'] = $this->movementTransformer->toResource($amount['movement']);
+        foreach($amounts as $i => $amount) {
+            $amount[$i]['movement'] = $this->movementTransformer->toResource($amount['movement']);
         }
         $response = new Response();
         $response->amounts = array_reverse($amounts);
