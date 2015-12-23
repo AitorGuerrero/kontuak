b74cff8580d1ac366a1c78ea970c0f6b134924b6
Aitor Guerrero <aitor.guerrero@atrapalo.com>
2015-12-23 23:32:52 +0100

    Movement resource completed

--- Ports/Resource/Movement.php
+++ Ports/Resource/Movement.php
@@ -6,10 +6,19 @@ class Movement
 {
     /** @var string */
     private $id;
+    /** @var float */
+    private $amount;
+    /** @var string */
+    private $concept;
+    /** @var \DateTime */
+    private $date;
 
     public function __construct(\Kontuak\Movement $movement)
     {
         $this->id = $movement->id()->toString();
+        $this->amount = $movement->amount();
+        $this->concept = $movement->concept();
+        $this->date = $movement->date();
     }
 
     /**
@@ -18,5 +27,29 @@ class Movement
     public function id()
     {
         return $this->id;
+    }
+
+    /**
+     * @return float
+     */
+    public function amount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @return string
+     */
+    public function concept()
+    {
+        return $this->concept;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function date()
+    {
+        return $this->date;
     }
 }
--- Tests/Ports/PeriodicalMovement/Update/Test.php
+++ Tests/Ports/PeriodicalMovement/Update/Test.php
@@ -26,7 +26,7 @@ class Test extends \PHPUnit_Framework_TestCase
 
     /** @var Source */
     public $source;
-    /** @var  */
+    /** @var Update */
     public $useCase;
     /** @var Request */
     public $request;
